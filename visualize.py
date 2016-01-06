0d53bb4b75caf5e1ee41e256a41591872af18e43
Chandler <use.an.alias@gmail.com>
2016-01-05 19:45:54 -0800

    Fix visualization bug

--- visualize.py
+++ visualize.py
@@ -11,20 +11,34 @@ class AccuracyLossPlot(Callback):
         self.val_losses = []
         self.val_accs = []
 
-    def on_batch_end(self, batch, logs={}):
-        self.train_losses.append(logs.get('loss'))
-        self.train_accs.append(logs.get('acc'))
+    def on_epoch_begin(self, epoch, logs={}):
+        self.train_losses.append([])
+        self.train_accs.append([])
+
+    def on_batch_end(self, epoch, logs={}):
+        self.train_losses[-1].append(logs.get('loss'))
+        self.train_accs[-1].append(logs.get('acc'))
+
+    def on_epoch_end(self, epoch, logs={}):
         self.val_losses.append(logs.get('val_loss'))
         self.val_accs.append(logs.get('val_acc'))
 
     @staticmethod
     def _plot_pair(train, test, label):
-        t = range(len(train))
-        line_train = plt.plot(t, train, label='Train')[0]
-        line_test = plt.plot(t, test, label='Validation')[0]
-        plt.legend(handles=[line_train, line_test])
-        plt.xlabel('Batch Number')
-        plt.ylabel(label)
+        epochs = len(train)
+        batches_per_epoch = len(train[0])
+        max_t = epochs * batches_per_epoch
+        batch_t = np.arange(max_t)
+        epoch_t = np.arange(0, max_t, batches_per_epoch)
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        ax.plot(batch_t, np.array(train), label='Train')
+        print(epoch_t.shape)
+        print(np.array(test).shape)
+        ax.plot(epoch_t, np.array(test), label='Validation')
+        ax.set_xlabel('Batch Number')
+        ax.set_ylabel(label)
+        ax.legend()
         plt.show()
 
     def on_train_end(self, logs={}):
@@ -46,5 +60,6 @@ class FirstWeightPlot(Callback):
         gridspec = GridSpec(4, 8)
         for index, kernel in enumerate(squeezed):
             ax = fig.add_subplot(gridspec[index])
+            ax.axis('off')
             ax.matshow(kernel)
         plt.show()
